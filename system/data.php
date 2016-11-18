46e88d551477e4bfc5cec51a34acc7df2888e6d3
Lena Gächter <gaechter.lena@gmail.com>
2016-11-18 15:04:00 +0100

    formatiert

--- system/data.php
+++ system/data.php
@@ -1,19 +1,19 @@
 <?php
-    function get_db_connection()
-    {
-        $db = mysqli_connect('localhost', '717416_3_1', 'T9VXwZxtsGS7', '717416_3_1')
-         or die('Fehler beim Verbinden mit dem MySQL-Server.');
-        mysqli_query($db, "utf8");
-        return $db;
-    }
+  function get_db_connection()
+  {
+      $db = mysqli_connect('localhost', '717416_3_1', 'T9VXwZxtsGS7', '717416_3_1')
+       or die('Fehler beim Verbinden mit dem MySQL-Server.');
+      mysqli_query($db, "utf8");
+      return $db;
+  }
 
-    function get_result($sql)
-    {
-      $db = get_db_connection();
-    //  echo $sql;
-      $result = mysqli_query($db, $sql);
-      mysqli_close($db);
-      return $result;
+  function get_result($sql)
+  {
+    $db = get_db_connection();
+  //  echo $sql;
+    $result = mysqli_query($db, $sql);
+    mysqli_close($db);
+    return $result;
     }
 
 
@@ -57,11 +57,11 @@
 /* ************************************************************************** */
 
 
-function get_fragen($f_id)
-{
-  $sql = "SELECT * FROM fragen WHERE f_id = $f_id;";
-  return get_result($sql);
-}
+  function get_fragen($f_id)
+  {
+    $sql = "SELECT * FROM fragen WHERE f_id = $f_id;";
+    return get_result($sql);
+  }
 
 
 
