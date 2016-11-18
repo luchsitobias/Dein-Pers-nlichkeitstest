63797ea748a44c7f1ae0550bc35a62ba7d7d5fcc
Sina Lou Ravasio <sl.ravasio@hispeed.ch>
2016-11-18 10:47:40 +0100

    no message

---
+++ backend_resultate.php
@@ -0,0 +1,126 @@
+<?php
+require_once ('system/data.php');
+require_once ('system/security.php');
+
+$error = false;
+$error_msg = "";
+$success = false;
+$success_msg = "";
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Backend - Auswerten</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/backend.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!--Navigation-->
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-12">
+
+                <ul class="nav nav-tabs navigation">
+                    <li>
+                        <a href="backend_resultate.php" role="button" aria-haspopup="true" aria-expanded="false">
+                            Resultate <span class=""></span>
+                        </a>
+                    </li>
+                    <li role="">
+                        <a href="backend_fragen.php" role="button" aria-haspopup="true" aria-expanded="false">
+                            Fragen <span class=""></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="backend_antworten.php" role="button" aria-haspopup="true" aria-expanded="false">
+                            Antworten<span class=""></span>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+    <!--Tabelle-->
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-12 tabelle">
+
+                <!--Titel-->
+                <h2>Resultate</h2>
+
+                <!--Tabelle-->
+                <table class="table table-bordered ">
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Anteil Resultat 1</th>
+                            <th>Anteil Resultat 2</th>
+                            <th>Anteil Resultat 3</th>
+                            <th>Anteil Resultat 4</th>
+                            <th>Löschen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">sl.ravasio@hispeed.ch</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><button class="btn btn-default">Löschen</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">sl.ravasio@hispeed.ch</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><button class="btn btn-default">Löschen</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">sl.ravasio@hispeed.ch</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><button class="btn btn-default">Löschen</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+        </div>
+    </div>
+</div>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -77,7 +77,7 @@ $success_msg = "";
                                 </div>
                                 <div class="form-group">
                                     <div class="col-sm-offset-2 col-sm-10">
-                                    <button href="backend_auswerten.php" type="submit" class="">Anmelden</button>
+                                    <button href="backend_resultate.php" type="submit" class="">Anmelden</button>
                                     </div>
                                 </div>
                                 <div style="margin-top:10%;" class="alert alert-danger" role="alert">Bitte fülle alle Felder korrekt aus</div>
