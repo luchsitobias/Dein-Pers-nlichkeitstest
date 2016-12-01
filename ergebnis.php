fb6feb72bfa68165eed53b54e53bfae7842c8274
Lena Gächter <gaechter.lena@gmail.com>
2016-12-01 11:10:42 +0100

    antworten in db speichern

--- ergebnis.php
+++ ergebnis.php
@@ -14,9 +14,7 @@ $error_msg = "";
 $success = false;
 $success_msg = "";
 
-foreach($_POST as $key => $value){
-  echo "Schreibe in die DB (Tabelle user_antworten) $user_id und $value<br>";
-}
+
 
  ?>
 
